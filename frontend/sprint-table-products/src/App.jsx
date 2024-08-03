0864ae97dafa7865988a3ff13e23916e72befa0e
shivupatil <shivupatil747@gmail.com>
2024-08-03 20:17:15 +0530

    added delete, edit functionalites

--- frontend/sprint-table-products/src/App.jsx
+++ frontend/sprint-table-products/src/App.jsx
@@ -10,6 +10,7 @@ import Home from './pages/Home';
 import Dashboard from './components/Dashboard';
 import { AuthProvider } from './components/AuthContext';
 import AddCategory from './pages/AddCategory';
+import EditCategory from './pages/EditCategory';
 
 function App() {
     return (
@@ -27,7 +28,8 @@ function App() {
                         <Route path="products" element={<Products />} />
                     </Route>
                     <Route path="/reset-password" element={<ResetPasswordPage />} />
-                    <Route path="/add-category" element={<AddCategory />} />
+                    <Route path="/home/category/add-category" element={<AddCategory />} />
+                    <Route path="/home/category/edit-category/:id" element={<EditCategory />} />
                 </Routes>
             </Router>
         </AuthProvider>
--- frontend/sprint-table-products/src/components/Header.jsx
+++ frontend/sprint-table-products/src/components/Header.jsx
@@ -7,19 +7,17 @@ const Header = () => {
     let logoutRef=useRef();
     const ShowLogoutPage=()=>{
         console.log(logoutRef);
-        if (logoutRef.current) {
-            if(logoutRef.current.style.display==="none"){
-               logoutRef.current.style.display = 'block';
-            }else{
-                logoutRef.current.style.display = 'none';
-            }
+              logoutRef.current.style.display = 'block';
         }
-    }
+
+    const cancelLogout=()=>{
+       logoutRef.current.style.display = 'none';
+        }
     return (
         <div className="header">
             <div className="header-logo">TableSprint</div>
             <div className="header-profile-icon" onClick={ShowLogoutPage}><RiAccountCircleLine/></div>
-            <div className="logoutDiv" ref={logoutRef}><Logout/></div> 
+            <div className="logoutDiv" ref={logoutRef}><Logout cancel={cancelLogout}/></div> 
         </div>
     );
 };
