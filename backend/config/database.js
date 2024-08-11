20acdfdd73f72e2178e5394bb85a77900ab466ff
shivupatil <shivupatil747@gmail.com>
2024-08-11 18:41:04 +0530

    upadated mongo_URI and added gitignore file

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,6 +5,8 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            connectTimeoutMS: 30000,  // 30 seconds
+            socketTimeoutMS: 45000  
         });
         console.log('MongoDB connected');
     } catch (error) {
