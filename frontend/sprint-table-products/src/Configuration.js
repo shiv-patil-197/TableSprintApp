6d86a14b8db995f353f684f12f8765c376292864
shivupatil <shivupatil747@gmail.com>
2024-08-15 12:48:27 +0530

    updatted base URL

--- frontend/sprint-table-products/src/Configuration.js
+++ frontend/sprint-table-products/src/Configuration.js
@@ -1,6 +1,6 @@
 const config = {
-    baseURL: "http://localhost:5100"
-    // baseURL: "https://tablesprintserver.onrender.com"
+    // baseURL: "http://localhost:5100"
+    baseURL: "https://tablesprintserver.onrender.com"
   };
 
   export {config}
